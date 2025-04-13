2a1cc4ad492552588171aa1b3eaafe9c40631057
Ashish Khankari <ashishkhankari0927@gmail.com>
2025-04-13 14:51:31 +0530

    lerned jwt tokens

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 app.get("/", (req, res) => {
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash("ashish@123", salt, (err, hash) => {
-      console.log(hash);
-    })
-  })
-  // res.cookie("name", "Ashish");
-  res.send("done");
+  let token = jwt.sign({ email: "ashish@gmail.com" }, "secret");
+  res.cookie("token", token);
+  res.send("send jwt token done");
 });
 
 app.get("/read", (req, res) => {
-    console.log(req.cookies)
+  console.log(req.cookies.token);
+  let data = jwt.verify(req.cookies.token, "secret");
+  console.log("data", data)
   res.send("read onlyy");
 });
 
